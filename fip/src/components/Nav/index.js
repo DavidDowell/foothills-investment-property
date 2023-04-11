00e93457b8a1ac7e7e0afea7cebbf601c3774113
Mitchell Armstrong <mra0211@gmail.com>
2023-04-10 20:13:51 -0400

    Integrated Tailwind CSS

--- fip/src/components/Nav/index.js
+++ fip/src/components/Nav/index.js
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/mortgagerelief">Mortgage Relief</Link></li>
-        <li><Link to="/agents">Agents</Link></li>
-        <li><Link to="/contact">Contact Us</Link></li>
+    <nav className="flex">
+      <ul className="flex">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/mortgagerelief">Mortgage Relief</Link>
+        </li>
+        <li>
+          <Link to="/agents">Agents</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
       </ul>
     </nav>
   );
--- fip/tailwind.config.js
+++ fip/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
