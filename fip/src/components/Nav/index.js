087dba99d445a884e21cb6f11c575b37c2f72f06
Mitchell Armstrong <mra0211@gmail.com>
2023-04-11 16:35:28 -0400

    Styled the nav bar some more

--- fip/src/components/Nav/index.js
+++ fip/src/components/Nav/index.js
@@ -9,17 +9,17 @@ const Nav = () => {
       </header>
       <div className="flex flex-row justify-between">
         <div id="logo"></div>
-        <ul className="nav-items flex justify-between items-center text-2xl px-8">
-          <li>
+        <ul className="nav-items flex justify-between items-center text-2xl px-8 mr-10">
+          <li className="nav-link">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="nav-link">
             <Link to="/mortgagerelief">Mortgage Relief</Link>
           </li>
-          <li>
+          <li className="nav-link">
             <Link to="/agents">Agents</Link>
           </li>
-          <li>
+          <li className="nav-link">
             <Link to="/contact">Contact Us</Link>
           </li>
         </ul>
