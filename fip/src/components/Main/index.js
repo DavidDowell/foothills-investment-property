9ddf7ec684955201b4d39f45753393eaac2140f0
DavidDowell <daviddowell1@gmail.com>
2023-04-12 16:21:20 -0400

    Created a validation for address and a name field

--- fip/src/components/Main/index.js
+++ fip/src/components/Main/index.js
@@ -1,20 +1,34 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import Swal from "sweetalert2";
 
 const Main = () => {
+
+  function validateAddress(address) {
+    // Address validation regex
+    const regex = /^\d+\s[A-z]+\s[A-z]+/;
+    return regex.test(address);
+  }
+
   const form = useRef();
   const checkInfo = (e) => {
     e.preventDefault();
     let userName = document.getElementById("user_name").value;
+    let address = document.getElementById("user_address").value;
     let userMessage = document.getElementById("user_message").value;
     if (!userName) {
       Swal.fire({
         icon: "warning",
-        title: "Must have an Address!",
+        title: "Must have a Name!",
+      });
+      console.log("Must have a Name!");
+    } else if (!validateAddress(address)) {
+      Swal.fire({
+        icon: "warning",
+        title: "Must have a valid Address!",
       });
       console.log("Must have an Address!");
-    }  else if (!userMessage) {
+    } else if (!userMessage) {
       Swal.fire({
         icon: "warning",
         title: "Must include a Message!",
@@ -45,7 +59,7 @@ const Main = () => {
             });
           }
         );
-        }
+    }
   };
   return (
     <div className="flex">
@@ -55,30 +69,46 @@ const Main = () => {
           <h2 className="font-bold text-5xl">SOLUTIONS</h2>
         </div>
         <div>
-            <form className="cf" ref={form} onSubmit={checkInfo}>
-              <div className="m-2 ">
-                <input
-                  type="text"
-                  placeholder="Address"
-                  name="user_name"
-                  id="user_name"
-                  className="text-zinc-950 user_name mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
-                  style={{width: '300px', height: '50px'}}
-                />
-              </div>
-              <div className="m-2">
-                <textarea
-                  name="message"
-                  type="text"
-                  placeholder="Tell us how we can help"
-                  id="user_message"
-                  className="text-zinc-950 user_message rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
-                  style={{width: '300px', height: '50px'}}
-                ></textarea>
-              </div>
-              <input className="bg-slate-100 p-5 rounded submit" type="submit" value="Submit" id="input-submit" style={{width: '300px'}} />
-            </form>
-          </div>
+          <form className="cf" ref={form} onSubmit={checkInfo}>
+          <div className="m-2">
+              <input
+                name="user_name"
+                type="text"
+                placeholder="What's your name?"
+                id="user_name"
+                className="pt-1 text-center text-zinc-950 user_name rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                style={{ width: "300px", height: "50px" }}
+              />
+            </div>
+            <div className="m-2">
+              <input
+                type="text"
+                placeholder="Address"
+                name="user_address"
+                id="user_address"
+                className="text-center text-zinc-950 user_address mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                style={{ width: "300px", height: "50px" }}
+              />
+            </div>
+            <div className="m-2">
+              <textarea
+                name="message"
+                type="text"
+                placeholder="Tell us how we can help"
+                id="user_message"
+                className="pt-1 text-center text-zinc-950 user_message rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                style={{ width: "300px", height: "50px" }}
+              ></textarea>
+            </div>
+            <input
+              className=" bg-blue-300 p-5 rounded submit hover:bg-inherit hover:text-white hover:text-2xl"
+              type="submit"
+              value="Submit"
+              id="input-submit"
+              style={{ width: "300px" }}
+            />
+          </form>
+        </div>
       </section>
       <section className="main-right mx-12">
         <div className="main-right-image"></div>
