33d3b56e536f5a590f7a6e3d3e4dcbfc5f0e1425
Mitchell Armstrong <mra0211@gmail.com>
2023-04-14 16:37:59 -0400

    Styled the Team section some more

--- fip/src/components/Team/index.js
+++ fip/src/components/Team/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Link, animateScroll } from 'react-scroll';
 import trainer from '../../assets/images/other/trainer-1.jpg';
+import trainer2 from '../../assets/images/other/trainer-2.jpg';
+import trainer3 from '../../assets/images/other/trainer-3.jpg';
 
 const Team = () => {
   return (
     <section id="Team">
-      <div>Meet The Team</div>
-      <div className="team grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="team-header">Meet The Team</div>
+      <div className="team grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
         <article class="team-member">
           <img
             src={trainer}
             alt="Arron Stephens in his workout clothes, ready to pump iron"
           />
           <div class="member-bio">
-            <h3 class="member-name">Arron Stephens</h3>
-            <h4 class="member-role">Speed / Strength</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              animi amet modi blanditiis culpa? Assumenda, blanditiis veniam
-              nulla similique temporibus cupiditate magni. Consequuntur ipsum
-              veritatis consequatur natus explicabo ratione deserunt!
-            </p>
+            <h3 class="member-name">Mike McNeely</h3>
+            <h4 class="member-role">Owner / President</h4>
+            <br />
+            <h5>Contact:</h5>
+            <h4>mike@noble-oak.com</h4>
           </div>
         </article>
         <article class="team-member">
@@ -29,14 +28,11 @@ const Team = () => {
             alt="Arron Stephens in his workout clothes, ready to pump iron"
           />
           <div class="member-bio">
-            <h3 class="member-name">Arron Stephens</h3>
-            <h4 class="member-role">Speed / Strength</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              animi amet modi blanditiis culpa? Assumenda, blanditiis veniam
-              nulla similique temporibus cupiditate magni. Consequuntur ipsum
-              veritatis consequatur natus explicabo ratione deserunt!
-            </p>
+            <h3 class="member-name">Logan McNeely</h3>
+            <h4 class="member-role">Management / Sales</h4>
+            <br />
+            <h5>Contact:</h5>
+            <h4>logan@noble-oak.com</h4>
           </div>
         </article>
         <article class="team-member">
@@ -45,62 +41,37 @@ const Team = () => {
             alt="Arron Stephens in his workout clothes, ready to pump iron"
           />
           <div class="member-bio">
-            <h3 class="member-name">Arron Stephens</h3>
-            <h4 class="member-role">Speed / Strength</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              animi amet modi blanditiis culpa? Assumenda, blanditiis veniam
-              nulla similique temporibus cupiditate magni. Consequuntur ipsum
-              veritatis consequatur natus explicabo ratione deserunt!
-            </p>
+            <h3 class="member-name">Jeremy Munro</h3>
+            <h4 class="member-role"> Acquisitions / Sales</h4>
+            <br />
+            <h5>Contact:</h5>
+            <h4>jeremy@noble-oak.com</h4>
           </div>
         </article>
         <article class="team-member">
           <img
-            src={trainer}
+            src={trainer2}
             alt="Arron Stephens in his workout clothes, ready to pump iron"
           />
           <div class="member-bio">
-            <h3 class="member-name">Arron Stephens</h3>
-            <h4 class="member-role">Speed / Strength</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              animi amet modi blanditiis culpa? Assumenda, blanditiis veniam
-              nulla similique temporibus cupiditate magni. Consequuntur ipsum
-              veritatis consequatur natus explicabo ratione deserunt!
-            </p>
+            <h3 class="member-name">Jennifer Travis</h3>
+            <h4 class="member-role">IT / Acquisitions</h4>
+            <br />
+            <h5>Contact:</h5>
+            <h4>jennifer@noble-oak.com</h4>
           </div>
         </article>
         <article class="team-member">
           <img
-            src={trainer}
+            src={trainer3}
             alt="Arron Stephens in his workout clothes, ready to pump iron"
           />
           <div class="member-bio">
-            <h3 class="member-name">Arron Stephens</h3>
-            <h4 class="member-role">Speed / Strength</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              animi amet modi blanditiis culpa? Assumenda, blanditiis veniam
-              nulla similique temporibus cupiditate magni. Consequuntur ipsum
-              veritatis consequatur natus explicabo ratione deserunt!
-            </p>
-          </div>
-        </article>
-        <article class="team-member">
-          <img
-            src={trainer}
-            alt="Arron Stephens in his workout clothes, ready to pump iron"
-          />
-          <div class="member-bio">
-            <h3 class="member-name">Arron Stephens</h3>
-            <h4 class="member-role">Speed / Strength</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-              animi amet modi blanditiis culpa? Assumenda, blanditiis veniam
-              nulla similique temporibus cupiditate magni. Consequuntur ipsum
-              veritatis consequatur natus explicabo ratione deserunt!
-            </p>
+            <h3 class="member-name">David Dowell</h3>
+            <h4 class="member-role">IT / Software Development</h4>
+            <br />
+            <h5>Contact:</h5>
+            <h4>david@noble-oak.com</h4>
           </div>
         </article>
       </div>
