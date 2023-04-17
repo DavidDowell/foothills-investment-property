fd370d2192791c224b74db0e180bb5f347663ba5
Mitchell Armstrong <mra0211@gmail.com>
2023-04-16 20:21:17 -0400

    Changed the form in the main section and also changed the emailjs template for this form

--- fip/src/components/Main/index.js
+++ fip/src/components/Main/index.js
@@ -14,25 +14,38 @@ const Main = () => {
   const form = useRef();
   const checkInfo = e => {
     e.preventDefault();
-    let userName = document.getElementById('user_name').value;
+    let userNameFirst = document.getElementById('user_name_first').value;
+    let userNameLast = document.getElementById('user_name_last').value;
+    let userPhone = document.getElementById('user_phone_number').value;
+    let userEmail = document.getElementById('user_email').value;
     let address = document.getElementById('user_address').value;
+    let zipCode = document.getElementById('user_zip').value;
     let userMessage = document.getElementById('user_message').value;
-    if (!userName) {
+    if (!userNameFirst && !userNameLast) {
       Swal.fire({
         icon: 'warning',
-        title: 'Please Provide Your Name!',
+        title: 'Please Provide Your Full Name!',
         allowOutsideClick: true,
         allowEnterKey: true,
       });
       console.log('Must have a Name!');
-    } else if (!validateAddress(address)) {
+    } else if (!validateAddress(address) && !zipCode) {
       Swal.fire({
         icon: 'warning',
-        title: 'Please Provide A Valid Address!',
+        title: 'Please Provide A Valid Address With A Zip Code!',
         allowOutsideClick: true,
         allowEnterKey: true,
       });
       console.log('Must have an Address!');
+    } else if (!userPhone || !userEmail) {
+      Swal.fire({
+        icon: 'warning',
+        title:
+          'Please Provide A Phone Number Or An Email So We Can Contact You Back',
+        allowOutsideClick: true,
+        allowEnterKey: true,
+      });
+      console.log('Must have contact phone!');
     } else if (!userMessage) {
       Swal.fire({
         icon: 'warning',
@@ -57,8 +70,12 @@ const Main = () => {
               title: 'Message Sent Successfully',
             });
             // Clearing the input fields so the suer doesn't have to reload the page for it to clear
-            document.getElementById('user_name').value = '';
+            document.getElementById('user_name_first').value = '';
+            document.getElementById('user_name_last').value = '';
+            document.getElementById('user_phone_number').value = '';
+            document.getElementById('user_email').value = '';
             document.getElementById('user_address').value = '';
+            document.getElementById('user_zip').value = '';
             document.getElementById('user_message').value = '';
 
             setShowForm(false);
@@ -97,42 +114,68 @@ const Main = () => {
             </button>
           ) : (
             <form className="cf" ref={form} onSubmit={checkInfo}>
-              <div className="m-2">
+              <div className="m-2 flex">
                 <input
-                  name="user_name"
+                  name="user_name_first"
                   type="text"
-                  placeholder="What's your name?"
-                  id="user_name"
-                  className="pt-1 text-center text-zinc-950 user_name mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
-                  style={{ width: '300px', height: '50px' }}
+                  placeholder="First Name"
+                  id="user_name_first"
+                  className="pt-1 mx-2 relief-form-name text-center text-zinc-950 mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                />
+                <input
+                  name="user_name_last"
+                  type="text"
+                  placeholder="Last Name"
+                  id="user_name_last"
+                  className="pt-1 mx-2 relief-form-name text-center text-zinc-950 mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
                 />
               </div>
-              <div className="m-2">
+              <div className="m-2 flex">
+                <input
+                  name="user_phone_number"
+                  type="text"
+                  placeholder="Phone Number"
+                  id="user_phone_number"
+                  className="pt-1 mx-2 relief-form-phone text-center text-zinc-950 mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                />
+                <input
+                  name="user_email"
+                  type="text"
+                  placeholder="Email"
+                  id="user_email"
+                  className="pt-1 mx-2 relief-form-email text-center text-zinc-950 mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                />
+              </div>
+              <div className="m-2 flex">
                 <input
                   type="text"
                   placeholder="Address"
                   name="user_address"
                   id="user_address"
-                  className="text-center text-zinc-950 user_address mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
-                  style={{ width: '300px', height: '50px' }}
+                  className="relief-form-address mx-2 text-center text-zinc-950 mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
+                />
+                <input
+                  type="text"
+                  placeholder="Zip Code"
+                  name="user_zip"
+                  id="user_zip"
+                  className="relief-form-zip mx-2 text-center text-zinc-950 mb-2 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
                 />
               </div>
               <div className="m-2">
                 <textarea
-                  name="message"
+                  name="user_message"
                   type="text"
                   placeholder="Tell us how we can help"
                   id="user_message"
-                  className="pt-1 text-center text-zinc-950 user_message rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
-                  style={{ width: '300px', height: '50px' }}
+                  className="relief-form-message pt-1 text-center text-zinc-950 rounded focus:bg-indigo-50 focus:ring-1 focus:ring-indigo-900"
                 ></textarea>
               </div>
               <input
-                className=" bg-blue-300 p-5 rounded submit hover:bg-inherit hover:text-white hover:text-2xl"
+                className="relief-form-submit p-5 bg-emerald-200"
                 type="submit"
                 value="Submit"
                 id="input-submit"
-                style={{ width: '300px' }}
               />
             </form>
           )}
