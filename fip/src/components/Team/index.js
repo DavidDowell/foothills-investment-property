035eeae19beeca9af37fe9ac5adead4fbcdbfed0
DavidDowell <daviddowell1@gmail.com>
2023-05-02 15:05:02 -0400

    added a few of our photos in place of avatars

--- fip/src/components/Team/index.js
+++ fip/src/components/Team/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { Link, animateScroll } from 'react-scroll';
-import David from '../../assets/images/other/DavidAvatar.svg';
-import Jen from '../../assets/images/other/JenAvatar.svg';
+import David from '../../assets/images/other/RealDave.JPG';
+import Jen from '../../assets/images/other/RealJen.jpeg';
 import Jeremy from '../../assets/images/other/MunroAvatar2.svg';
-import Logan from '../../assets/images/other/LoganAvatar.svg';
+import Logan from '../../assets/images/other/RealLogan.jpeg';
 import Mike from '../../assets/images/other/MikeAvatar.svg';
 import Deena from '../../assets/images/other/DeenaAvatar.svg'
 
@@ -14,7 +14,7 @@ const Team = () => {
       <div className="team-header">Meet The Team</div>
       <div className="team grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
         <article class="team-member">
-          <img src={Mike} />
+          <img className='avatar' src={Mike} />
           <div class="member-bio">
             <h3 class="member-name">Mike McNeely</h3>
             <h4 class="member-role">Owner / President</h4>
@@ -24,7 +24,7 @@ const Team = () => {
           </div>
         </article>
         <article class="team-member">
-          <img src={Logan} />
+          <img className='avatar' src={Logan} />
           <div class="member-bio">
             <h3 class="member-name">Logan McNeely</h3>
             <h4 class="member-role">Management / Sales</h4>
@@ -34,7 +34,7 @@ const Team = () => {
           </div>
         </article>
         <article class="team-member">
-          <img src={Jeremy} />
+          <img className='avatar' src={Jeremy} />
           <div class="member-bio">
             <h3 class="member-name">Jeremy Munro</h3>
             <h4 class="member-role"> Acquisitions / Sales</h4>
@@ -44,7 +44,7 @@ const Team = () => {
           </div>
         </article>
         <article class="team-member">
-          <img src={Jen} />
+          <img className='avatar' src={Jen} />
           <div class="member-bio">
             <h3 class="member-name">Jennifer Travis</h3>
             <h4 class="member-role">HR / Acquisitions</h4>
@@ -54,7 +54,7 @@ const Team = () => {
           </div>
         </article>
         <article class="team-member">
-          <img src={David} />
+          <img className='avatar' src={David} />
           <div class="member-bio">
             <h3 class="member-name">David Dowell</h3>
             <h4 class="member-role">IT / Software Development</h4>
@@ -64,7 +64,7 @@ const Team = () => {
           </div>
         </article>
         <article class="team-member">
-          <img src={Deena} />
+          <img className='avatar' src={Deena} />
           <div class="member-bio">
             <h3 class="member-name">Deena Mayberry</h3>
             <h4 class="member-role">Office Manager</h4>
