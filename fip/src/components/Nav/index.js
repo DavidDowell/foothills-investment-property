86794b6b8f1fa30134bd2c05cedb51e1c439e639
DavidDowell <daviddowell1@gmail.com>
2023-05-12 15:36:25 -0400

    new hamurger svg

--- fip/src/components/Nav/index.js
+++ fip/src/components/Nav/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-scroll';
-import logo from '../../assets/images/logo/FinalOakTree.png';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+import logo from "../../assets/images/logo/FinalOakTree.png";
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,21 +11,21 @@ const Nav = () => {
       const scrollTop =
         window.pageYOffset || document.documentElement.scrollTop;
       const mortgageReliefSectionOffsetTop =
-        document.getElementById('MortgageRelief').offsetTop;
+        document.getElementById("MortgageRelief").offsetTop;
       setIsPastMortgageRelief(scrollTop > mortgageReliefSectionOffsetTop);
     }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <nav
       className={`svg-img navbar ${
-        isPastMortgageRelief ? 'scrolled' : 'bg-transparent'
+        isPastMortgageRelief ? "scrolled" : "bg-transparent"
       }`}
     >
       <div className="svg-img-2"></div>
@@ -36,22 +36,38 @@ const Nav = () => {
         <div className="flex md:hidden mt-6 ml-6">
           <button
             className={` text-center fixed text-neutral-900 opacity-80 focus:opacity-40 bg-white rounded ${
-              menuOpen ? 'rotate-180' : ''
+              menuOpen ? "rotate-180" : ""
             }`}
             onClick={() => setMenuOpen(!menuOpen)}
           >
-            <svg viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6">
-              <path
-                fillRule="evenodd"
-                d="M2.5 5h15a.5.5 0 0 1 0 1h-15a.5.5 0 1 1 0-1zm0 5h15a.5.5 0 0 1 0 1h-15a.5.5 0 1 1 0-1zm0 5h15a.5.5 0 0 1 0 1h-15a.5.5 0 1 1 0-1z"
-              />
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLincap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z"
+                  fill="#000000"
+                ></path>{" "}
+              </g>
             </svg>
           </button>
         </div>
 
         <ul
           className={`menu nav-items md:flex md:justify-between items-center font-bold md:text-xl lg:text-3xl text-lg px-8 mr-10 ${
-            menuOpen ? 'block' : 'hidden'
+            menuOpen ? "block" : "hidden"
           } transition-all duration-{#} ease-in-out`}
         >
           <li className="nav-link hover:text-yellow-300 hover:-translate-y-1">
