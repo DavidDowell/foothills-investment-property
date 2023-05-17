3d5c96be6274c56c9c573736396199359c8ae8c8
DavidDowell <daviddowell1@gmail.com>
2023-05-17 11:25:46 -0400

    fixed up the WWD section

--- fip/src/components/WWD/index.js
+++ fip/src/components/WWD/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const WhatWeDo = () => {
   return (
-    <div className='mt-20'>
-        <div className="top-house">
-            <h1 className='text-left text-3xl'>What We Do</h1>
-        </div>
-        <div className='top-mid-house'/>
-        <div className='mid-house' />
-        <div className='low-mid-house' />
-        <div className='bottom-house' />
-        <div className='ground' />
+    <div className='mt-20 reno-container'>
+        <h1 className='text-left text-6xl p-4'>What We Do</h1>
+        <div className="top-house grayscale hover:grayscale-0"/>
+        <div className='top-mid-house grayscale hover:grayscale-0'/>
+        <div className='mid-house grayscale hover:grayscale-0' />
+        <div className='low-mid-house grayscale hover:grayscale-0' />
+        <div className='bottom-house grayscale hover:grayscale-0' />
     </div>
   );
 };
